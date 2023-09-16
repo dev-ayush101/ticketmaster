c7310c9a39a957403f4a48001d229b5da8fa466d
dev-ayush101 <ayushmishraofficial.619@gmail.com>
2023-09-17 02:30:11 +0530

    Added global exception handler

---
+++ src/main/java/com/ticketmaster/ticketmaster/config/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.ticketmaster.ticketmaster.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+    }
+}
